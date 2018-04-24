3d2fad7896b41587a2fca0dcb45e0813885faaad
Daners <danielespinosa.bin@gmail.com>
2018-04-24 16:40:15 -0500

    server socket

--- server.js
+++ server.js
@@ -2,16 +2,41 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var http = require('http');
-
 var app = express();
+
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'src')));
 
+var messages = [{
+	author: "Carlos",
+    text: "Hola! que tal?"
+},{
+	author: "Pepe",
+    text: "Muy bien! y tu??"
+},{
+	author: "Paco",
+    text: "Genial!"
+}];
+
+io.on('connection', function(socket) {
+	console.log('Un cliente se ha conectado');
+
+  console.log(socket.id);
+    socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+    io.sockets.emit('messages', messages);
+  });
+});
+
 
 var port =process.env.PORT || '8000';
 app.set('port', port);
-
-var server = http.createServer(app);
 server.listen(port);
